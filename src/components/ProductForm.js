b339ae1fc372461150eb771e78c0ddb63a3e12bb
syeef <syeefislam@yahoo.com>
2024-01-10 18:34:04 +0530

    Product form created

---
+++ src/components/ProductForm.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+const ProductForm = ({ addProduct }) => {
+  const [id, setId] = useState("");
+  const [price, setPrice] = useState("");
+  const [product_name, setProductName] = useState("");
+  const [category, setCategory] = useState("electronics");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newProduct = {
+      id,
+      price,
+      product_name,
+      category,
+    };
+    addProduct(newProduct);
+    setId("");
+    setPrice("");
+    setProductName("");
+    setCategory("electronics");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Product Id"
+        value={id}
+        onChange={(e) => setId(e.target.value)}
+      />
+      <input
+        type="number"
+        placeholder="Selling Price"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <input
+        type="text"
+        placeholder="Product Name"
+        value={product_name}
+        onChange={(e) => setProductName(e.target.value)}
+      />
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="electronics">Electronics</option>
+        <option value="food">Foods</option>
+        <option value="skincare">Skincare</option>
+      </select>
+      <button type="submit">Add Product</button>
+    </form>
+  );
+};
+
+export default ProductForm;
